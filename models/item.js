fcf70165c03f9522bc1484c0741871f2e84f7a3a
Frank <fmeza07@gmail.com>
2015-03-05 14:11:09 -0800

    refactor existing code and add new item properties

--- models/item.js
+++ models/item.js
@@ -2,11 +2,10 @@ Auction.Item = DS.Model.extend({
   text: DS.attr(),
   description: DS.attr(),
   owner: DS.attr(),
-  // image: DS.attr(),
-  // price: DS.attr(),
-  // backstory: DS.attr()
-  // year-aquired: DS.attr(),
-  // bid-number: DS.attr(),
-  // owner: DS.attr();
+  image: DS.attr(),
+  price: DS.attr(),
+  backstory: DS.attr(),
+  yearAcquired: DS.attr(),
+  owner: DS.attr(),
   category: DS.belongsTo('category', {async: true})
 });
